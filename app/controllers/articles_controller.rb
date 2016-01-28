aa004bd45d5a851486886b61690b559d502eaa04
zfa9 <zfa9@sfu.ca>
2016-01-28 12:14:06 -0800

    a2 change1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
@@ -18,10 +18,10 @@ class ArticlesController < ApplicationController
 
   @article.save
   redirect_to @article
-end
-
-private
-  def article_params
-    params.require(:article).permit(:title, :text)
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
